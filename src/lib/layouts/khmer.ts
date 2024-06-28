9f33d45810da7d78a0c516a8ded04efbf7f7efd6
dkc-1379 <3117538764@qq.com>
2024-06-28 11:20:31 +0800

    Add Khmer keyboard layout

---
+++ src/lib/layouts/khmer.ts
@@ -0,0 +1,38 @@
+import { LayoutItem } from "../interfaces";
+
+/**
+ * Layout: Khmer
+ * Source: dkc (https://github.com/dkc-1379)
+ */
+export default <LayoutItem>{
+  layout: {
+    default: [
+      "« ១ ២ ៣ ៤ ៥ ៦ ៧ ៨ ៩ ០ គ ធ {bksp}",
+      "{tab} ឆ ឹ េ រ ត យ ុ ិ ោ ផ ៀ ឨ ឮ",
+      "{lock} ា ស ដ ថ ង ហ ្ ក ល ើ ់ {enter}",
+      "{shift} \\ ឋ ខ ច វ ប ន ម ំុ ។ ៊ {shift}",
+      ".com @ {space}",
+    ],
+    lock: [
+      "« ១ ២ ៣ ៤ ៥ ៦ ៧ ៨ ៩ ០ គ ធ {bksp}",
+      "{tab} ឈ ឺ ែ ឬ ទ ួ ូ ី ៅ ភ ៀ ឨ ឮ",
+      "{lock} ាំ ៃ ឌ ធ ឣ ះ ញ គ ឡ ើ ់ {enter}",
+      "{shift} \\ ឍ ឃ ជ េះ ព ណ ំ ំុ ។ ៊ {shift}",
+      ".com @ {space}",
+    ],
+    shift: [
+      "» ! ៗ \" ៛ % ៌ ័ ៏ ( ) ៝ ឪ {bksp}",
+      "{tab} ឈ ឺ ែ ឬ ទ ួ ូ ី ៅ ភ ឿ ឧ ឭ",
+      "{lock} ាំ ៃ ឌ ធ ឣ ះ ញ គ ឡ ោៈ ៉ {enter}",
+      "{shift} / ឍ ឃ ជ េះ ព ណ ំ ុះ ៕ ? {shift}",
+      ".com @ {space}",
+    ],
+    'shift-lock': [
+      "» ! ៗ \" ៛ % ៌ ័ ៏ ( ) ៝ ឪ {bksp}",
+      "{tab} ឆ ឹ េ រ ត យ ុ ិ ោ ផ ឿ ឧ ឭ",
+      "{lock} ា ស ដ ថ ង ហ ្ ក ល ោៈ ៉ {enter}",
+      "{shift} / ឋ ខ ច វ ប ន ម ុះ ៕ ? {shift}",
+      ".com @ {space}",
+    ],
+  },
+};
